55af04cb8be43884ed72750503f9463d1249c3bf
PaulaAraya <paula.arayawilliams@gmail.com>
2018-07-18 14:16:20 -0300

    avances función editar

--- public/js/post.js
+++ public/js/post.js
@@ -1,15 +1,15 @@
 // POST USUARIO
 function posting() {
   // POST INICIO
-  sendPost.addEventListener("click", () => {
+  sendPost.addEventListener('click', () => {
     const currentUser = firebase.auth().currentUser;
     const postAreaText = postArea.value;
-    postArea.value = " ";
+    postArea.value = ' ';
     //Para tener una nueva llave en la colección messages
     let newPostKey = firebase
       .database()
       .ref()
-      .child("post")
+      .child('post')
       .push().key;
 
     firebase
@@ -27,11 +27,11 @@ const drawPosts = (posts) => {
   postPrint.innerHTML = '';
   Object.entries(posts.val()).forEach((post) => {
     postPrint.innerHTML += `
-            <ul class="list-group list-group-flush" style="width: 100%;"> 
-              <li class="list-group-item">
-              <h6 class="card-title">${post[1].creator}</h6>
-              <p class="card-text text-justify">${post[1].text}</p>
-              <i class="fas fa-edit" data-text="${post[1].text}" onclick="updatePost("eventUpdate") id="editPost"> </i> <i class="fas fa-trash-alt" data-post="${post[0]}" onclick="deletePost(event)"></i> 
+            <ul class='list-group list-group-flush' style='width: 100%;'> 
+              <li class='list-group-item'>
+              <h6 class='card-title'>${post[1].creator}</h6>
+              <p class='card-text text-justify' class='posts'>${post[1].text}</p>
+              <i class='fas fa-edit' data-text='${post[0]}' onclick='updatePost(event)'></i> <i class='fas fa-trash-alt' data-post='${post[0]}' onclick='deletePost(event)'> </i> 
               </li>
             </ul>
         `;
@@ -40,8 +40,8 @@ const drawPosts = (posts) => {
 //Borrar Post
 function deletePost(event) {
   event.stopPropagation(); //se activa solamente donde se hace click
-  const postId = event.target.getAttribute("data-post");
-  firebase.database().ref("/post").child(postId).remove()
+  const postId = event.target.getAttribute('data-post');
+  firebase.database().ref('post/').child(postId).remove()
     .then(function () {
       console.log('El documento ha sido borrado');
     })
@@ -50,33 +50,24 @@ function deletePost(event) {
       console.error('Error al editar el borrado', error);
     });
 }
-/*
-// Editar post
-function updatePost(eventUpdate) {
-  /*
-  document.getElementById("postArea").value = postAreaText;
-  const btnEdit = document.getElementById("sendPost");
-  btnEdit.innerHTML = "EDITAR";
-  btnEdit.onclick = function () {
-    const postText = eventUpdate.target.getAttribute('data-text');
-    let postAreaText = document.getElementById("postArea").value
-    return firebase.database().ref("/post").child(postText).update({
-      text: postAreaText
-    }).then(function () {
-      console.log('El documento ha sido editado');
-      btnEdit.innerHTML = "SEND";
-    })
-      .catch(function (error) {
-        // The document probably doesn't exist.
-        console.error('Error al editar el documento', error);
+//Editar Post
+function updatePost(event) {
+  eventUpdate.stopPropagation();
+  const textId= event.target.getAttribute('data-text');
+  firebase.database().ref('post/' + textId).once("value", function(post){
+    const postAreaText = (post.val().text);
+    document.getElementsByClassName("postTextArea").value = postAreaText;
+    const btnEdit = document.getElementById("sendPost");
+    btnEdit.innerHTML = "EDITAR";
+    btnEdit.onclick = function(){
+      firebase.database().ref('post').child(textId).update({
+        text: postAreaText
       });
-  }
-};
-
-/*
-//contador
-let contador = 0;
-mundoLike.onclick = function() {
-  printLike.innerHTML= contador++;
+    }
+  });
+  //no puedo usar ide en post 
+  //data-text en todas parte donde se me plasca text area pongo id que corresponde a ese post por medio de una classname.
+  
 }
-*/
+
+
